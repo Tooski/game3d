4c5b2cd512b76e7da6ce33df17a69ab5f4b49798
Josef Nosov <nosovj@uw.edu>
2014-08-23 17:44:08 -0700

    First Push

---
+++ public_html/js/gui.js
@@ -0,0 +1,34 @@
+//var gui;
+//
+//$(document).ready(function() {
+//
+//    gui.fps = (function() {
+//        var fps = $('#fps');
+//        return {
+//            update: function(frames) {
+//                fps.text(frames + " fps");
+//            }
+//        };
+//    });
+//}
+//);
+
+var stats = new Stats();
+stats.setMode(1); // 0: fps, 1: ms
+
+// Align top-left
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+
+document.body.appendChild( stats.domElement );
+
+setInterval( function () {
+
+    stats.begin();
+
+    // your code goes here
+
+    stats.end();
+
+}, 1000 / 60 );
